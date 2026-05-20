12cf96a114437efe0e8ed554a170e55dcc420582
nabievadiana05-ai <nabievadiana05@gmail.com>
2026-05-20 12:56:04 +0400

    Add files via upload

---
+++ Lab1_dynamic/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, Nabieva Diana Maximovna!" << endl;
+    return 0;
+}
---
+++ Lab1_static/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello, Nabieva Diana Maximovna!" << endl;
+
+    return 0;
+}
